40a1ed656540d48e25d31776781731f9e38dc90c
Student <user@chromebox-001.(none)>
2013-07-02 16:27:06 -0700

    Initial Commit

--- hackbright_app.py
+++ hackbright_app.py
@@ -7,14 +7,43 @@ def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
     row = DB.fetchone()
-    print """\
-Student: %s %s
-Github account: %s"""%(row[0], row[1], row[2])
+    if row == None:
+        print "Student does not exist."
+    else:
+        print """\
+    Student: %s %s
+    Github account: %s"""%(row[0], row[1], row[2])
 
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
+
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" %(first_name, last_name)
+
+
+def add_project(title, description, max_grade):
+    query = """INSERT into Projects values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added Project: %s %s %s" %(title, description, max_grade)
+
+def get_project_title(title):
+    query = """SELECT * FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    if row == None:
+        print "Title doesn't exist."
+    else:
+        print """\
+    Title: %s
+    Description: %s
+    Max Grade: %s """ %(row[0], row[1], row[2])
+
 
 def main():
     connect_to_db()
@@ -26,9 +55,31 @@ def main():
         args = tokens[1:]
 
         if command == "student":
-            get_student_by_github(*args) 
+            if len(args)==1:
+                get_student_by_github(*args) 
+            else:
+                print "incorrect arguments, correct format is: student <github>. "
         elif command == "new_student":
-            make_new_student(*args)
+            if len(args)==3:
+                make_new_student(*args)
+            else:
+                print "incorrect arguments, correct format is: new_student <fname> <lname> <github>"
+        elif command == "projects":
+            if len(args)== 1:
+                get_project_title(*args)
+            else:
+                print "Incorrect argument, correct format is: projects <title>"
+        elif command == "project+":             #checks for string arg
+            if len(args) > 3:
+                title= tokens[1]
+                description = " ".join(tokens[2:-1]) #everything in between is the string
+                max_grade = tokens[-1]
+                add_project(title, description, max_grade)
+            else:
+                print """Incorrect number of arguments, correct format is: 
+                project+ <title> <description> <max_grade>"""
+        else:
+            print "That was not a valid command."
 
     CONN.close()
 
